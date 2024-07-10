7c1e3d10eda513349d49bbc80febe90288d474b1
Feuerraderr <belov-vlad2008@yandex.ru>
2024-07-10 13:42:29 +0300

    interface

---
+++ LogIn/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  LogIn
+//
+//  Created by Владислав Резван on 10.07.2024.
+//
+
+import Foundation
+import UIKit
+
+class GradientView: UIView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    private func setupGradient() {
+        self.layer.addSublayer(gradientLayer)
+        gradientLayer.colors = [UIColor.purple.cgColor, UIColor.blue.cgColor]
+    }
+}
---
+++ LogIn/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  LogIn
+//
+//  Created by Владислав Резван on 09.07.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
